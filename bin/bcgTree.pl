6493287c1ffdcbeba8ae599dd33329e5a05a260e
Markus Ankenbrand <markus@ankenbrand.me>
2015-10-23 08:52:05 +0200

    add raxml repo and check

--- bin/bcgTree.pl
+++ bin/bcgTree.pl
@@ -95,7 +95,7 @@ pod2usage( -msg => "No proteome specified. Use --proteome name=file.fa", -verbos
 pod2usage( -msg => 'hmmsearch not in $PATH and binary not specified use --hmmsearch-bin', -verbose => 0, -exitval => 1 ) unless ($opt_hmmsearch_bin);
 pod2usage( -msg => 'muscle not in $PATH and binary not specified use --muscle-bin', -verbose => 0, -exitval => 1 ) unless ($opt_muscle_bin);
 pod2usage( -msg => 'Gblocks not in $PATH and binary not specified use --gblocks-bin', -verbose => 0, -exitval => 1 ) unless ($opt_gblocks_bin);
-#pod2usage( -msg => 'raxmlHPC not in $PATH and binary not specified use --raxml-bin', -verbose => 0, -exitval => 1 ) unless ($opt_raxml_bin);
+pod2usage( -msg => 'raxmlHPC not in $PATH and binary not specified use --raxml-bin', -verbose => 0, -exitval => 1 ) unless ($opt_raxml_bin);
 
 # init a root logger in exec mode
 Log::Log4perl->init(
