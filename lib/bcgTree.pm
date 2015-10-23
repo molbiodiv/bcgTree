e7be24a12d42c2f4dc6573de7ab7305f7e06db7c
Markus Ankenbrand <markus@ankenbrand.me>
2015-10-23 12:03:11 +0200

    debug problems with seqfilter on travis

--- lib/bcgTree.pm
+++ lib/bcgTree.pm
@@ -127,10 +127,10 @@ sub get_sequences_of_best_hmm_hits{
 	my $separator = $self->{'separator'};
 	$L->info("Collecting sequences of best hits from hmmsearch for each gene.");
 	foreach my $gene (@genes){
-		my $cmd = "$FindBin::RealBin/../SeqFilter/bin/SeqFilter --ids-rename='s/$separator/ /' --desc-replace --line-width 0 $out/all.concat.fa --ids $out/$gene.ids --out $out/$gene.fa";
+		my $cmd = "$FindBin::RealBin/../SeqFilter/bin/SeqFilter --ids-rename='s/$separator/ /' --desc-replace --line-width 0 $out/all.concat.fa --ids $out/$gene.ids --out $out/$gene.fa 2>&1";
 		$L->info($cmd);
 		my $result = qx($cmd);
-		$L->debug($result);
+		$L->logdie($result);
 	}
 	$L->info("Finished collection of sequences of best hits from hmmsearch.");
 }
