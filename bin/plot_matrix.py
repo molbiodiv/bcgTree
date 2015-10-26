0d96582516836f2709a75eb97da0ac0b72f924f5
Markus Ankenbrand <markus@ankenbrand.me>
2015-10-26 14:56:50 +0100

    add python script to plot an absence presence matrix from a csv file

---
+++ bin/plot_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+plt.clf()
+fig=plt.figure()
+ax = fig.add_subplot(111)
+mat=pandas.read_csv(sys.argv[1],header=0,index_col=0)
+ax.matshow(mat,cmap=cm.gray, interpolation='nearest', vmin=0, vmax=1)
+ax.xaxis.set_major_locator(MultipleLocator(1))
+ax.set_xticklabels([""]+mat.columns.tolist(), rotation=90)
+ax.yaxis.set_major_locator(MultipleLocator(1))
+ax.yaxis.set_major_formatter(FormatStrFormatter('%s'))
+ax.set_yticklabels([""]+mat.index.tolist())
+ax.tick_params(axis='both', which='both', labelsize=4, direction='out',
+                       labelleft='on', labelright='off', labelbottom='off',
+                       labeltop='on', left='on', right='off', bottom='off',
+                       top='on')
+plt.savefig(sys.argv[2], bbox_inches='tight')
+
