dfdc54f7c7d3597377308ee679ccda307c5fc3be
Markus Ankenbrand <markus@ankenbrand.me>
2015-10-27 14:47:10 +0100

    Increase figure size and maxticks for plot_matrix.py

--- bin/plot_matrix.py
+++ bin/plot_matrix.py
@@ -7,13 +7,15 @@ import pandas
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 plt.clf()
-fig=plt.figure()
+fig=plt.figure(figsize=(40, 40))
 ax = fig.add_subplot(111)
 mat=pandas.read_csv(sys.argv[1],header=0,index_col=0)
 # min=0 and max=1 is set to just distinguish absent and present
 # remove those parameters to see colors scaled by value
 ax.matshow(mat,cmap=cm.gray, interpolation='nearest', vmin=0, vmax=1)
-ax.xaxis.set_major_locator(MultipleLocator(1))
+locator=MultipleLocator(1)
+locator.MAXTICKS = 10000
+ax.xaxis.set_major_locator(locator)
 ax.set_xticklabels([""]+mat.columns.tolist(), rotation=90)
 ax.yaxis.set_major_locator(MultipleLocator(1))
 ax.yaxis.set_major_formatter(FormatStrFormatter('%s'))
