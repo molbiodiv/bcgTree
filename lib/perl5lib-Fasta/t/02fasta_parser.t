1204f9133f371dba9c0e515e78f31cb19d0d1d6e
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-22 15:17:50 +0100

    Add Fasta and Verbose modules from Thomas Hackl

---
+++ lib/perl5lib-Fasta/t/02fasta_parser.t
@@ -0,0 +1,157 @@
+#!/usr/bin/env perl
+
+# $Id$
+
+use strict;
+use warnings;
+
+use Test::More;
+use Data::Dumper;
+
+use FindBin qw($RealBin);
+use lib "$RealBin/../lib/";
+
+BEGIN { use_ok('Fasta::Parser'); }
+
+my $Class = 'Fasta::Parser';
+my ($fp, $fpr, $fpr_string);
+
+#--------------------------------------------------------------------------#
+
+=head2 sample data
+
+=cut
+
+(my $Dat_file = $FindBin::RealScript) =~ s/t$/dat/; # data
+(my $Dmp_file = $FindBin::RealScript) =~ s/t$/dmp/; # data structure dumped
+(my $Tmp_file = $FindBin::RealScript) =~ s/t$/tmp/; # data structure dumped
+
+my $Dat = do { local $/; local @ARGV = $Dat_file; <> }; # slurp data to string
+my @Dat = split(/(?<=\n)(?=>)/, $Dat);
+
+my @Dmp = do "$Dmp_file"; # read and eval the dumped structure
+
+
+# new
+subtest 'new and DESTROY' => sub{
+	$fp = new_ok($Class);
+	
+	#  default attributes
+	is($fp->{mode}, '<', 'Default attribute "mode"');
+	is($fp->{file}, undef, 'Default attribute "file"');
+	is($fp->{_buffer}, undef, 'Default attribute "_buffer"');
+	
+	# DESTROY closes fh
+	my $fh = $fp->{fh};
+	$fp = undef;
+
+	# read from file
+	$fpr = new_ok($Class, [
+		mode => '<', 
+		file => $Dat_file,
+	]);
+	#  default attributes
+	is($fpr->{mode}, '<', 'Custom attribute "mode"');
+	is($fpr->{file}, $Dat_file, 'Custom attribute "file"');
+	
+		# large file
+	# create large file
+	my $fasta_large = ($Dat)x10000;
+	$fpr_string = Fasta::Parser->new(file => \$fasta_large);
+	
+};
+
+
+subtest 'filehandle' => sub{
+	# fh
+	can_ok($fpr, 'fh');
+	can_ok($fpr, 'is_fh');
+	my $fprh = $fpr->{fh};
+	is($fpr->fh, $fprh, 'Get $obj->fh()');
+	is($fpr->is_fh, 0, '$obj->is_fh() FILE');
+	ok($fpr->is_fh('FILE'), '$obj->is_fh("FILE")');
+	
+	is($fpr->fh(\*STDIN), \*STDIN, 'Set $obj->fh()');
+	is($fpr->is_fh, 1, '$obj->is_fh() PIPE');
+	ok($fpr->is_fh('PIPE'), '$obj->is_fh("PIPE")');
+	
+	is($fpr->fh($fprh), $fprh , 'Reset $obj->fh() FILE');
+	is($fpr->is_fh, 0, '$obj->is_fh()');
+
+	is($fpr_string->is_fh(), 2, '$obj->is_fh() SCALAR');	
+	ok($fpr_string->is_fh("SCALAR"),'$obj->is_fh("SCALAR")');	
+	
+};
+
+# next_seq
+subtest 'next_seq' => sub{
+	can_ok($fpr, 'next_seq');
+
+	for(my $i=0; $i<@Dat; $i++){
+		is($fpr->next_seq->string, $Dat[$i], 'Get $obj->next_seq()');	
+	}
+	is($fpr->next_seq, undef, 'Get $obj->next_seq() eof');
+	is($fpr->next_seq->string, $Dat[0], 'Get $obj->next_seq() restart after eof');
+};
+
+# check_format
+subtest 'check_format' => sub{
+	can_ok($fpr, 'check_format');
+	is(Fasta::Parser->new(file => \("Not a fasta record"))->check_format, 
+		undef, '$obj->check_format on not FASTA is undef');
+	my $fp = Fasta::Parser->new(file => $Dat_file);
+	isa_ok($fp->check_format, $Class, '$obj->check_format on FASTA');
+	isa_ok($fp->check_format, $Class, '$obj->check_format on FASTA');
+};
+
+# seek
+
+# sample_seqs
+subtest 'sample_seqs' => sub{
+	can_ok($fpr, 'sample_seqs');
+	my @sample_seqs = $fpr->sample_seqs(2);
+	is(scalar @sample_seqs, 2, '$obj->sample_seqs()');
+	isa_ok($_, 'Fasta::Seq') for @sample_seqs;
+	@sample_seqs = $fpr->sample_seqs(3);
+	is(scalar @sample_seqs, 3, '$obj->sample_seqs()');
+	isa_ok($_, 'Fasta::Seq') for @sample_seqs;
+	@sample_seqs = $fpr->sample_seqs(10);
+	is(scalar @sample_seqs, 3, '$obj->sample_seqs()');
+	isa_ok($_, 'Fasta::Seq') for @sample_seqs;
+	
+	@sample_seqs = $fpr_string->sample_seqs(10);
+	is(scalar @sample_seqs, 10, '$obj->sample_seqs() large file');
+	isa_ok($_, 'Fasta::Seq') for @sample_seqs;
+};
+
+# guess_seq_length
+
+# guess_seq_count
+
+# check_fh_is_pipe
+
+
+subtest 'new write parser' => sub{
+	# writer
+	my $fpw = new_ok($Class, [
+		mode => '+>', 
+		file => $Tmp_file,
+	]);
+	
+	#  default attributes
+	is($fpw->{mode}, '+>', 'Writing mode "mode"');
+	is($fpw->{file}, $Tmp_file, 'Writer mode "file"');
+	is(-e $Tmp_file, 1, 'Create custom file'); # copy of STDIN needs to be greater than 3
+	
+	unlink $Tmp_file;
+};
+
+# append_seq
+
+# append_tell
+
+
+
+
+
+done_testing();
---
+++ lib/perl5lib-Verbose/lib/Verbose/ProgressBar.pm
@@ -0,0 +1,344 @@
+package Verbose::ProgressBar;
+
+use warnings;
+use strict;
+
+# preference libs in same folder over @INC
+use lib '../';
+
+use Verbose;
+
+use IO::File;
+use IO::Uncompress::Gunzip;
+
+our $VERSION = '0.09';
+
+$|++;
+
+
+
+##------------------------------------------------------------------------##
+
+=head1 NAME 
+
+Verbose::ProgressBar - Progress Visualisation.
+
+=cut
+
+=head1 DESCRIPTION
+
+Visualizes progress in an self-updating pv like ProgressBar, inlcuding ETA
+ calculation.
+
+=cut
+
+=head1 SYNOPSIS
+
+
+  # Counter based progress
+  my $iter = 10000;
+  my $pg = Verbose::ProgressBar->new(
+    size => $iter
+  )
+  
+  my $i;
+  for($i=0;$i++;$i<$iter){
+  	$i++;
+  	$pg->update($i)
+  }
+  $pg->finish($i);
+  
+  # File size based progress
+  open(my FILE, "my.file");
+  my $pg = Verbose::ProgressBar->new(
+    size => $fh,
+    line_width => 120,
+  )
+  # parse large file linewise
+  my $c=0;
+  while(<FILE>){
+    # invoke update every 10000th line
+    $pg->update unless $c%10000;
+  };
+  $pg->finish;
+
+
+=cut
+
+=head1 CHANGELOG
+
+=head2 0.08
+
+=over
+
+=item [Feature] Added C<level> and C<report_level> feature to modify behaviour
+ according to given verbose level.
+
+=item [BugFix] TTS and ETA now also show number of days, if required.
+
+=item [BugFix] TTS is now always printed in case finish is called.
+
+=item [Change] Preference libs in same folder over @INC
+
+=item [Change] Added svn:keywords
+
+=back
+
+=over
+
+=item 0.07
+
+[BugFix] Update with undefined/null value created error.
+
+Bugfix. Initial bar was one space short.
+
+=item 0.06 [Thomas Hackl 2012-10-10]
+
+C<< $pb->finish >> resets the ProgressBar so that it can be used again, e.g.
+ if the same file is read again.
+
+=item 0.05
+
+After C<< $pb->finish() >> call, ETA field in output is replaced by TTS 
+(total time spent).
+
+=item 0.04
+
+Bugfix, C<< $self->{value} >> was not set if exteral value is provided.
+
+=item 0.03
+
+Cooldown for C<< $pb->update() >> of one second. Prevents overhead if update
+ is called very often.
+
+=item 0.02
+
+Added ETA. Added C<< $pb->finish() >> for finishing the output. Added 
+ "endless" monitoring for unknown size.
+
+=item 0.01
+
+Initial Verbose::ProgressBar module. Provides constructor and 
+ update methods. Computes and print a ProgressBar.
+
+=back
+
+=head1 Constructor METHOD
+
+=cut
+
+
+sub new{
+	my $class = shift;
+	
+	# overwrite defaults
+	my $self = {
+		fh => \*STDERR,
+		size => undef,		# number, file or fh
+		line_width => 80,	#
+		level => 1,
+		report_level => 1, 
+		_fh =>undef, 		# a monitored file handle
+		_bins => undef,
+		_bin => -1,
+		_bin_time => undef,
+		_start_time => undef,
+		_time => 0,
+		_template => undef,
+		@_%2 ? (size => shift, @_) : @_	# overwrite + odd number of params -> first is message
+	};
+	
+	# calculate max size if filehandle
+	if(defined ($self->{size}) && ref $self->{size}){
+		$self->{_fh} = $self->{size};
+		$self->{size} = -s $self->{_fh};
+	}
+
+	# calculate number of bins 
+	$self->{_bins} = $self->{line_width} - 23; # 7 PBval, 3 PBbar, 13 PBeta
+	$self->{_template} = "\r%6s [%-".($self->{_bins}+1)."s]";
+
+	bless $self, $class;
+}
+
+
+=head1 Class METHODS
+
+=head2 update
+
+NOTE: The update function has a cooldown of 1 second to prevent 
+ computational overhead, still it can cost you some seconds. 
+ If this is a problem, performance can be further increased using
+ an external counter and invoking update only every x iterations.
+
+  # parse large file linewise
+  my $c=0;
+  while(<FILE>){
+  	 # invoke update every 10000th line
+  	 $pg->update unless $c%10000;
+  };
+
+=cut
+
+sub update{
+	my ($self, $value) = @_;
+	
+	# short circuit if less than 1 second
+	(my $time = time)-$self->{_time} || return;
+	$self->{_start_time} || ($self->{_start_time} = $time);
+	
+	$self->{level} > $self->{report_level} && return;
+	
+	if (!defined ($value) && defined($self->{_fh})){
+		$self->{value} = tell($self->{_fh});
+	}elsif(!$value){
+		$self->{value} = $value = 0;
+	}else{
+		$self->{value} = $value;
+	}
+	
+	# progress bar with known size
+	
+	my $eta;
+	if($self->{size}){
+		# calculate current bin
+		my $bin = int($self->{value} /$self->{size} * $self->{_bins});
+
+		# return unless new bin
+		return if $bin <= $self->{_bin} && $self->{_time} > -1;
+		$self->{_bin} = $bin;
+		
+		# initialize
+		# 
+		unless ($bin){
+			print {$self->{fh}} "    0  [".( " "x ($self->{line_width} - 22))."]";
+			return;
+		}
+		
+		# (re)calculate bin time
+		$self->{_bin_time} = ($time-$self->{_start_time})/$bin;
+		if($self->{_time} < 0){ # reset by finish
+			my $ela_time = $time-$self->{_start_time};		
+			$eta = $ela_time >= 86400 
+				? sprintf(" TTS %dd %02d:%02d:%02d", (gmtime($ela_time))[7,2,1,0])
+				: sprintf(" TTS %02d:%02d:%02d", (gmtime($ela_time))[2,1,0])
+		}elsif(my $eta_time = ($self->{_bins}-$bin) * $self->{_bin_time}){
+			$eta = $eta_time >= 86400 
+				? sprintf(" ETA %dd %02d:%02d:%02d", (gmtime($eta_time))[7,2,1,0])
+				: sprintf(" ETA %02d:%02d:%02d", (gmtime($eta_time))[2,1,0])
+		}else{
+			$eta = '';
+		}
+		printf {$self->{fh}} 
+			($self->{_template}.$eta,
+				Verbose->Humanize($self->{value}),		# PGval
+				'='x($self->{_bin}).'>',		# PGbar
+			);
+		
+	}else{
+		# fill/reset bins in endless loop
+		if($self->{_bin} < $self->{_bins}){
+			$self->{_bin}++
+		}else{
+			$self->{_bin} = 0;
+		}
+		
+		if(defined $value){
+			$self->{value} = $value;
+		}else{
+			# use simple incrementor if no values are provided
+			$self->{value}++ 
+		}
+		# print progress line
+		printf {$self->{fh}} 
+		($self->{_template},
+			Verbose->Humanize($self->{value}),		# PGval
+			' 'x($self->{_bin}-2).'<=>',		# PGbar
+		);
+	}
+	
+	# save time	
+	$self->{_time} = $time;
+}
+
+=head2 finish
+
+Call after monitoring is finished. Requires the same input as 
+ C<update>, which means none in case a file handle is monitored or
+ the final counter value in case an external counter is used.
+Finishes the output.
+
+Returns the final size and the total time required. Resets the bar, so it 
+ can be used again.
+
+=cut
+
+sub finish{
+	my ($self, $value) = @_;
+	$self->{level} > $self->{report_level} && return;
+	# run the last bin, if it hasn't been run. Can happen if resolution is
+	# small und update on the final bin is not triggered
+	$self->{_time} = -1; # disable short circuit
+	$self->update($value);
+	print {$self->{fh}} "\n";
+	my @re = ($self->{value}, time()-$self->{_start_time});
+	
+	# reset
+	$self->{_bin} = -1,
+	$self->{_bin_time} = undef;
+	$self->{_start_time} = undef;
+	$self->{_time} = 0;
+	
+	
+	return @re;
+}
+
+
+=head1 Accessor METHODS
+
+=cut
+
+=head2 fh
+
+Get/set the file handle the progress bar is printed to.
+
+=cut
+
+sub fh{
+	my ($self, $fh) = @_;
+	$self->{fh} = $fh if defined($fh);
+	return $self->{fh}
+}
+
+=head2 size
+
+Get/set size.
+
+=cut
+
+sub size{
+	my ($self, $size) = @_;
+	$self->{size} = $size if defined($size);
+	return $self->{size}
+}
+
+=head1 private METHODS
+
+=cut
+
+=head2 eta
+
+=cut
+
+
+
+
+=head1 AUTHOR
+
+Thomas Hackl S<thomas.hackl@uni-wuerzburg.de>
+
+=cut
+
+
+
+1;
