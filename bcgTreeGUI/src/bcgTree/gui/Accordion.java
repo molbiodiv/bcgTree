42acdc5b6c7f8b4dbb6b6c686fcb64b28ca8a8ab
Markus Ankenbrand <markus@ankenbrand.me>
2016-02-23 23:03:17 +0100

    Improve accordion layout

--- bcgTreeGUI/src/bcgTree/gui/Accordion.java
+++ bcgTreeGUI/src/bcgTree/gui/Accordion.java
@@ -1,12 +1,14 @@
 package bcgTree.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
 
 import org.jdesktop.swingx.JXCollapsiblePane;
 
@@ -25,12 +27,15 @@ public class Accordion extends JPanel {
 		this.setLayout(new BorderLayout());
 		JXCollapsiblePane collapsiblePane = new JXCollapsiblePane();
 		collapsiblePane.add(comp);
+		collapsiblePane.setCollapsed(true);
+		this.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		// get the built-in toggle action
 		Action toggleAction = collapsiblePane.getActionMap().get(JXCollapsiblePane.TOGGLE_ACTION);
 		// use the collapse/expand icons from the JTree UI
 		toggleAction.putValue(JXCollapsiblePane.COLLAPSE_ICON, UIManager.getIcon("Tree.expandedIcon"));
 		toggleAction.putValue(JXCollapsiblePane.EXPAND_ICON, UIManager.getIcon("Tree.collapsedIcon"));
 		JButton col = new JButton(collapsiblePane.getActionMap().get(JXCollapsiblePane.TOGGLE_ACTION));
+		col.setHorizontalAlignment(JButton.LEFT);
 		col.setText(this.title);
 		this.add(col, BorderLayout.NORTH);
 		this.add(collapsiblePane, BorderLayout.CENTER);
