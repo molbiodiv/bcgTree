bf9f1da23bad73f990dce366e0782cd26e42520b
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-17 14:03:30 +0100

    Increase version number to 1.0.5

--- bcgTreeGUI/src/bcgTree/gui/BcgTree.java
+++ bcgTreeGUI/src/bcgTree/gui/BcgTree.java
@@ -41,7 +41,7 @@ import javax.swing.UIManager.LookAndFeelInfo;
 
 public class BcgTree extends JFrame {
 	private static final long serialVersionUID = 1L;
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 
 	public static void main(String[] args) {
 		BcgTree mainWindow = new BcgTree();
--- lib/bcgTree.pm
+++ lib/bcgTree.pm
@@ -11,7 +11,7 @@ use File::Path qw(make_path);
 use File::Spec;
 use Fasta::Parser;
 
-our $VERSION = '1.0.4';
+our $VERSION = '1.0.5';
 
 my $L = Log::Log4perl::get_logger();
 
